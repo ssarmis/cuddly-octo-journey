eb5a007b2c0e049f60bd2f8a9eb5bc7dc81e9a69
streanga-sarmis <streangasarmis@gmail.com>
2020-06-19 16:39:59 +0300

    added saving before opening file in window

--- src/panel_actions.h
+++ src/panel_actions.h
@@ -9,6 +9,15 @@ void openFileAction(void* data0, void* data1){
     GapBuffer* filenameBuffer = (GapBuffer*)data1;
     char* filename = gapToString(filenameBuffer);
 
+    if(window->buffer.filename){
+        // TODO(Sarmis) add some save thing
+        // even something like a logo or idk
+        // to indicate that someting was saved
+        // and make it global so it can be called from anywhere
+        gapWriteFile(&window->buffer);
+        TRACE("Saved file %s\n", window->buffer.filename);
+    }
+
     window->buffer = gapReadFile(filename);
 
     window->scrollX = 0;
