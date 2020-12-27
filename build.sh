c1bb95226befdf898d3711153b66312398ce2fe9
streanga-sarmis <streangasarmis@gmail.com>
2020-12-27 12:33:39 +0200

    added back the build for linux and made sure it works

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+# LINUX
 g++ -o app infernoGl/gl.cpp src/app.cpp \
-    -L/opt/homebrew/Cellar/sdl2/2.0.14/lib -I/opt/homebrew/Cellar/sdl2/2.0.14/include \
-    -IinfernoGl -lSDL2main -lSDL2 -DDEBUG_BUILD -std=c++11 \
-    -framework OpenGL \
+    -IinfernoGl -lGL -lSDL2main -lSDL2 -DDEBUG_BUILD -std=c++11 \
     -Wno-#warnings -Wno-switch -Wno-shift-negative-value -Wno-deprecated-declarations -Wno-format -Wno-write-strings -Wno-narrowing -g 
+
+# APPLE
+# g++ -o app infernoGl/gl.cpp src/app.cpp \
+#     -L/opt/homebrew/Cellar/sdl2/2.0.14/lib -I/opt/homebrew/Cellar/sdl2/2.0.14/include \
+#     -IinfernoGl -lSDL2main -lSDL2 -DDEBUG_BUILD -std=c++11 \
+#     -framework OpenGL \
+#     -Wno-#warnings -Wno-switch -Wno-shift-negative-value -Wno-deprecated-declarations -Wno-format -Wno-write-strings -Wno-narrowing -g 
